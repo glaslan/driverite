34502ff4c5ca374c63855c2b2fe55190ffb7f94e
ethanwaddell <ethanwaddell8@gmail.com>
2024-11-11 13:53:12 -0500

    Fixed TripPage Error

--- components/TripPage.tsx
+++ components/TripPage.tsx
@@ -1,3 +1,15 @@
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-paper";
+
 export default function TripPage() {
-    return(<><h1>A trip has started</h1></>);
+    return(<><Text variant="displayLarge" style={styles.background}>A trip has started</Text></>);
 };
+
+const styles = StyleSheet.create({
+    background: {
+      display: "flex",
+      width: "100%",
+      height: "100%",
+      backgroundColor: "white"
+    },
+});
