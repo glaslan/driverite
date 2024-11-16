24a5369713ba0a213548504bf192370ab61a1289
DylanGlass <glasd6702@gmail.com>
2024-11-15 20:13:02 -0500

    Recognize AB#3, and calculate corner AB#16

--- app/(tabs)/CorneringStats.tsx
+++ app/(tabs)/CorneringStats.tsx
@@ -1,49 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Gyroscope, DeviceMotion, DeviceMotionMeasurement } from 'expo-sensors'
+import { Text, View } from 'react-native';
+import { DeviceMotionSensor } from 'expo-sensors/build/DeviceMotion';
+import { DeviceMotionOrientation } from 'expo-sensors';
 
 export default function CorneringingStats() {
 
-    const [{ x, y, z }, setGyro] = useState({
-        x: 0,
-        y: 0,
-        z: 0,
-    });
-    const [subscription, setSubscription] = useState(null);
-
-    const subscribe = () => {
-        setSubscription(
-            Gyroscope.addListener(gyroscopeData => {
-                setGyro(gyroscopeData);
-            })
-        );
-    };
-
-    const [orientation, setorientation] = useState<DeviceMotionMeasurement | null>(null);
-    let previousGyro = [];
-    const maxStoredGyro = 10;
-
-    DeviceMotion.addListener(orientation);
-    if (orientation?.orientation == 0) {
-        // Get correct gyro number for portrait
-        if (x > 2) {
-            previousGyro.push(x);
-        }
-    } 
-    else if (orientation?.orientation == 90) {
-            // Get correct gyro number for right landscape
-            if (z > 2) {
-                previousGyro.push(z);
-            }
-    }
-    else if (orientation?.orientation == 180) {
-        // Get correct gyro number for left landscape
-        if (x < 2) {
-            previousGyro.push(x);
-        }
-}
+    const [gyroData, setGyroData] = useState({x: 0, y: 0, z: 0,});
+    const [gyroEnabled, setGyroEnabled] = useState(true);
+    const [phoneOrientation, setPhoneOrientation] = useState(0);
+    const [badCorneringInstances, setBadCorneringInstances] = useState(0);
+    const [totalCorneringInstances, setTotalCorneringInstances] = useState(0);
+    const [score, setScore] = useState(0);
+    const gyroDataRef = useRef(gyroData);
 
     useEffect(() => {
-        subscribe();
-    }, []);
+        gyroDataRef.current = gyroData;
+        let subscription;
+        if (gyroEnabled) {
+            subscription = Gyroscope.addListener(gyroscopeData => {
+                setGyroData(gyroscopeData);
+            });
+        } else {
+            subscription?.remove();
+        }
 
+        let orientation; 
+        orientation = DeviceMotion.addListener(phoneOrientation => {
+            setPhoneOrientation(phoneOrientation.orientation);
+        });
+
+
+        const intervalId = setInterval(() => {
+            if(Math.abs(gyroDataRef.current.y) > 0.5 && phoneOrientation === 0) {
+                setTotalCorneringInstances(prevtotal => prevtotal + 1);
+                if (gyroData.y > 2 || gyroData.y < -2)
+                    setBadCorneringInstances(badCorneringInstances + 1);
+            }
+        }, 100);
+        // Add to the total counter and badDriving counter when threshold corners occur
+        
+
+        setScore(50 + 50 * Math.sqrt(((totalCorneringInstances - badCorneringInstances) / totalCorneringInstances) - 0.5 ));
+
+        // Return for the useEffect hook
+        return () => {
+            subscription?.remove();
+            clearInterval(intervalId); 
+            score;
+        };
+    }, [gyroData, gyroEnabled, phoneOrientation, totalCorneringInstances, badCorneringInstances]);
+
+    return (
+        <View>
+            <Text>Cornering stats: x: {gyroData.x}, y: {gyroData.y}, z: {gyroData.z}</Text>
+            <Text>Total Corners: {totalCorneringInstances}</Text>
+            <Text>Total Bad Corners: {badCorneringInstances}</Text>
+            <Text>Orientation {phoneOrientation}</Text>
+        </View>
+    );
 }
