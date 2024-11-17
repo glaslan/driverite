b3f0ec6be1e4512614a9d580488fa722d2f4267e
ChrisHunt <cljhunt@icloud.com>
2024-11-17 13:00:49 -0500

    Created view end trip summary
    created view end trip summary, a screen that will pop up once the end trip button has been hit which will display the avg score, breaking avg, speed avg, acceleration avg, and cornering avg. along with a section that will show the user bad habits based off their scores.  Just a few problems left that i am more than willing to fix
    1. the errors that pop up are already explained in the file and i can fix later
    2. Once the main screen circular displays have been figured out i can change the displays that are being used
    3. there might have to be some changes to the stylesheets once 2 is done

---
+++ components/ViewEndTrip.tsx
@@ -0,0 +1,176 @@
+import React, { useState } from 'react';
+import {ScrollView ,Text, View, StyleSheet} from 'react-native';
+import CircularProgress from 'react-native-circular-progress-indicator';
+import Drivingstats from '@/app/(tabs)/DrivingStats';
+
+export default function ViewTripProgress(){
+    //const [genScore, setGeneralScore] = useState(average);
+    //const [brkScore, setBrakingScore] = useState(Braking);
+    //const [spdScore, setSpeedScore] = useState(Speed);
+    //const [accScore, setAccelerationScore] = useState(Acceleration);
+    //const [corScore, setCorneringScore] = useState(Cornering);
+
+    //once the values that are being used and what file they are coming from
+    //this can be changed as the rest of the errors that are left
+    //are related to this section above, i can fix this once they 
+    //location/'s have been set
+
+    const InfoCard = ({ typescore }: { typescore: string }) => {
+        let title;
+        let info;
+        if(typescore == 'braking'){
+            title = <Text style={styles.Title}>Braking</Text>
+            info = <Text style={styles.text}>Your braking is hard, we suggest taking your time with it.</Text>
+        }
+        if(typescore == 'speed'){
+            title = <Text style={styles.Title}>Speed</Text>
+            info = <Text style={styles.text}>Your speed isn't great, follow the speed limits displayed.</Text>
+        }
+        if(typescore == 'cornering'){
+            title = <Text style={styles.Title}>Cornering</Text>
+            info = <Text style={styles.text}>You seem to be taking turns badly, we suggest you take your time with it.</Text>
+        }
+        if(typescore == 'acceleration'){
+            title = <Text style={styles.Title}>Acceleration</Text>
+            info = <Text style={styles.text}>Your acceleration isn't great, accelerate at a different rate from what you were.</Text>
+        }
+        if(typescore == 'all-good'){
+            title = <Text style={styles.Title}>Great job</Text>
+            info = <Text style={styles.text}>According to the score calculators, your doing amazing!.</Text>
+        }
+        return (
+            <view>
+                {title}
+                {info}
+            </view>
+        );
+    };
+
+    const CircleDisplay = ({score , radivalue}: {score: number, radivalue: number}) =>{
+        return(
+            <CircularProgress
+                value={score}
+                radius={radivalue}
+                progressValueColor={'black'}
+                maxValue={100}
+                titleStyle={{ fontWeight: 'bold' }}
+                duration={250}
+                strokeColorConfig={[
+                    { color: '#9d1818', value: 0 },
+                    { color: 'red', value: 50 },
+                    { color: 'orange', value: 60 },
+                    { color: '#e7db43', value: 70 },
+                    { color: '#89e03b', value: 100 },
+                ]}
+            />
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.Title}>Trip Summary</Text>
+            <Text style={styles.Title}>Driving Scores</Text>
+            <View style={styles.progressContainer}>
+                <CircleDisplay
+                    score={genScore}
+                    radivalue={100}
+                />
+                <Text style={styles.text}>General rating</Text>
+                <View style={styles.progressrow}>
+                    <CircleDisplay
+                        score={accScore}
+                        radivalue={40}
+                    />
+                    <Text style={styles.text}>Acceleration rating</Text>
+                    <CircleDisplay
+                        score={spdScore}
+                        radivalue={40}
+                    />
+                    <Text style={styles.text}>Speed rating</Text>
+                </View>
+                <View style={styles.progressrow}>
+                    <CircleDisplay
+                        score={brkScore}
+                        radivalue={40}
+                    />
+                    <Text style={styles.text}>Braking rating</Text>
+                    <CircleDisplay
+                        score={corScore}
+                        radivalue={40}
+                    />
+                    <Text style={styles.text}>Cornering rating</Text>
+                </View>
+            </View>
+            <Text style={styles.Title}>Bad Habits</Text>
+            <ScrollView style={styles.scrollContainer}>
+                {brkScore < 70 && (
+                    <InfoCard
+                        typescore="braking"
+                    />
+                )}
+                {accScore < 70 && (
+                    <InfoCard
+                        typescore="acceleration"
+                    />
+                )}
+                {spdScore < 70 && (
+                    <InfoCard
+                        typescore="speed"
+                    />
+                )}
+                {corScore < 70 && (
+                    <InfoCard
+                        typescore="cornering"
+                    />
+                )}
+                {brkScore >= 70 && accScore >= 70 && spdScore >= 70 && corScore >= 70 && (
+                    <InfoCard 
+                        typescore="all-good"
+                    />
+                )}
+            </ScrollView>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    background: {
+      display: "flex",
+      width: "100%",
+      height: "100%",
+      backgroundColor: "white"
+    },
+    Title:{
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 15,
+    },
+    progressrow: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flex: 1,
+        paddingTop: 20,
+    },
+    progressContainer: {
+        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    scrollContainer:{
+        flex: 1,
+        height: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    text: {
+        fontSize: 15,
+        flexWrap: 'wrap',
+        maxWidth: '80%',
+    }
+});
